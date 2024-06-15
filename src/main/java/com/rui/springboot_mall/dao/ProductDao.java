d635d9bb6970d794fa6c03b891f9bd34397931c6
Ryanhuan <44796638+Ryanhuan@users.noreply.github.com>
2024-06-15 16:02:53 +0800

    商品功能 實作新增商品

--- src/main/java/com/rui/springboot_mall/dao/ProductDao.java
+++ src/main/java/com/rui/springboot_mall/dao/ProductDao.java
@@ -1,8 +1,11 @@
 package com.rui.springboot_mall.dao;
 
+import com.rui.springboot_mall.dto.ProductRequest;
 import com.rui.springboot_mall.model.Product;
 
 public interface ProductDao {
 
      Product getProductById(Integer productId);
+
+     Integer createProduct(ProductRequest productRequest);
 }
--- src/main/java/com/rui/springboot_mall/service/ProductService.java
+++ src/main/java/com/rui/springboot_mall/service/ProductService.java
@@ -1,9 +1,11 @@
 package com.rui.springboot_mall.service;
 
+import com.rui.springboot_mall.dto.ProductRequest;
 import com.rui.springboot_mall.model.Product;
 
 public interface ProductService {
 
     Product getProductById(Integer productId);
 
+    Integer createProduct(ProductRequest productRequest);
 }
