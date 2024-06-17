e8b3d913368d95a50734832b0991549641d822ca
Ryanhuan <44796638+Ryanhuan@users.noreply.github.com>
2024-06-17 23:57:15 +0800

    商品功能 實作查詢商品列表基礎實作

--- src/main/java/com/rui/springboot_mall/dao/ProductDao.java
+++ src/main/java/com/rui/springboot_mall/dao/ProductDao.java
@@ -3,7 +3,11 @@ package com.rui.springboot_mall.dao;
 import com.rui.springboot_mall.dto.ProductRequest;
 import com.rui.springboot_mall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+     List<Product> getProducts();
 
      Product getProductById(Integer productId);
 
--- src/main/java/com/rui/springboot_mall/service/ProductService.java
+++ src/main/java/com/rui/springboot_mall/service/ProductService.java
@@ -3,13 +3,17 @@ package com.rui.springboot_mall.service;
 import com.rui.springboot_mall.dto.ProductRequest;
 import com.rui.springboot_mall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
 
-    void updateProduct(Integer productId,ProductRequest productRequest);
+    void updateProduct(Integer productId, ProductRequest productRequest);
 
     void deleteProductById(Integer productId);
 }
